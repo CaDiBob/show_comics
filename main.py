5bcc02b40304c010d293eec8073cf957d70951d4
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-10 22:59:19 +0900

    Добавил работу с API ВКонтакте

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import requests
 
 from environs import Env
 
+def get_group_vk_numbers(vk_url, params):
+    response = requests.get(vk_url, params=params)
+    response.raise_for_status
+    return response.json()
+
+
 
 def comics_atribute(url):
     response = requests.get(url)
@@ -28,12 +34,23 @@ def main():
     env = Env()
     env.read_env('.env')
     vk_app_id = env('VK_APP_ID')
+    vk_group_id = env('VK_GROUP_ID')
+    vk_access_token = env('VK_ACCESS_TOKEN')
+
     url = 'https://xkcd.com/353/info.0.json'
     atribute = comics_atribute(url)
     img_url = atribute['img_url']
     title = atribute['title']
     download_image(img_url, title, params=None)
     print(atribute['comment'])
+
+    vk_url = 'https://api.vk.com/method/photos.getWallUploadServer'
+    params = {
+        "group_id": vk_app_id,
+        "access_token": vk_access_token,
+        "v": 5.131,
+    }
+    print(get_group_vk_numbers(vk_url, params))
 
 
 if __name__ == '__main__':
