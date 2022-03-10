53d6d64ee62c26ae56018dd8cd504d74f599b098
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-10 21:43:08 +0900

    Добавил возможность скачивать один комикс

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import requests
+
+from environs import Env
+
+
+def comics_atribute(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    comics_atribute = response.json()
+    # return comics_atribute
+
+    return {
+        'title': comics_atribute['safe_title'],
+        'img_url': comics_atribute['img'],
+        'comment': comics_atribute['alt']
+    }
+
+
+def download_image(url, title, params=None):
+    folder = 'comics'
+    os.makedirs(folder, exist_ok=True)
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    with open(os.path.join(folder, f'{title}.png'), 'wb') as file:
+        file.write(response.content)
+
+
+def main():
+    env = Env()
+    env.read_env('.env')
+    url = 'https://xkcd.com/353/info.0.json'
+    # print(comics_atribute(url))
+    # exit()
+    atribute = comics_atribute(url)
+    img_url = atribute['img_url']
+    title = atribute['title']
+    download_image(img_url, title, params=None)
+    print(atribute['comment'])
+
+
+if __name__ == '__main__':
+    main()
