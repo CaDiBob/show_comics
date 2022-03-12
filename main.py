eeb55c92db12d115e282e27448e770fd00467215
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-13 03:46:50 +0900

    PEP8 корректировка

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ def main():
         upload_parameters = get_upload_parameters(vk_access_token)
         upload_url = upload_parameters['response']['upload_url']
         result_upload = get_upload_result(upload_url, title)
-        
+
         save_wall_img = save_img_to_server(
             result_upload, vk_group_id, vk_access_token
         )
